98256316fbdb7854a867d696377b4d6e08ca2808
Craig McKeachie <craigdmckeachie@gmail.com>
2025-12-01 15:44:52 -0500

    code commit

---
+++ src/main/java/com/pluralsight/Program.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Program {
+
+    public static void main(String[] args) {
+        String name = "Slim Shady";
+        System.out.println("Hi, my name is, what? My name is, who? / My name is, chka-chka, "  + name);
+    }
+}
